0d27d08bd4f00c57e272b0a95b8603921f3afc42
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-04 17:21:35 +0800

    Add main import

---
+++ mod.ts
@@ -0,0 +1,2 @@
+export { default, ExFetch, exFetch, exFetchPaginate, type ExFetchEventName, type ExFetchEventOnRetryPayload, type ExFetchOptions, type ExFetchPaginateOptions, type ExFetchRetryOptions } from "./exfetch.ts";
+export { HTTPHeaderLink, type HTTPHeaderLinkEntry } from "./header/link.ts";
