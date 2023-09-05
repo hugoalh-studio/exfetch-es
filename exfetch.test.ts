89b8ae651f84e7bce8aa865fa29a6a500bd86c3c
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-05 11:38:05 +0800

    Fix permission

--- exfetch.test.ts
+++ exfetch.test.ts
@@ -1,6 +1,10 @@
 import { assert } from "https://deno.land/std@0.201.0/assert/assert.ts";
 import { ExFetch } from "./exfetch.ts";
-Deno.test("https://jsonplaceholder.typicode.com/posts", async () => {
+Deno.test("https://jsonplaceholder.typicode.com/posts", {
+	permissions: {
+		net: ["jsonplaceholder.typicode.com"]
+	}
+}, async () => {
 	const response = await new ExFetch().fetch("https://jsonplaceholder.typicode.com/posts");
 	assert(response.ok === true && response.status === 200);
 });
