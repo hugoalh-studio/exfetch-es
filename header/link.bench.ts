e6ef072e9c390c4d2a6cac3b235bbdc67f1b6303
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-05 11:53:52 +0800

    Update test

--- header/link.bench.ts
+++ header/link.bench.ts
@@ -1,8 +1,8 @@
 import { HTTPHeaderLink } from "./link.ts";
-Deno.bench("From String 1", () => {
+Deno.bench("From String 1", { permissions: "none" }, () => {
 	new HTTPHeaderLink(`<https://one.example.com>; rel="preconnect", <https://two.example.com>; rel="preconnect", <https://three.example.com>; rel="preconnect"`);
 });
-Deno.bench("From Entries 1", () => {
+Deno.bench("From Entries 1", { permissions: "none" }, () => {
 	new HTTPHeaderLink([
 		["https://one.example.com", { rel: "preconnect" }],
 		["https://two.example.com", { rel: "preconnect" }],
--- header/link.test.ts
+++ header/link.test.ts
@@ -1,21 +1,21 @@
 import { assertEquals } from "https://deno.land/std@0.201.0/assert/assert_equals.ts";
 import { assertThrows } from "https://deno.land/std@0.201.0/assert/assert_throws.ts";
 import { HTTPHeaderLink } from "./link.ts";
-Deno.test("From String Good 1", () => {
+Deno.test("From String Good 1", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink(`<https://example.com>; rel="preconnect"`);
 	assertEquals(instance.hasParameter("rel", "preconnect"), true);
 	assertEquals(instance.hasParameter("rel", "connect"), false);
 	assertEquals(instance.hasParameter("rel", "postconnect"), false);
 	assertEquals(instance.getByRel("preconnect")[0][0], "https://example.com");
 });
-Deno.test("From String Good 2", () => {
+Deno.test("From String Good 2", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink(`<https://example.com/%E8%8B%97%E6%9D%A1>; rel="preconnect"`);
 	assertEquals(instance.hasParameter("rel", "preconnect"), true);
 	assertEquals(instance.hasParameter("rel", "connect"), false);
 	assertEquals(instance.hasParameter("rel", "postconnect"), false);
 	assertEquals(instance.getByRel("preconnect")[0][0], "https://example.com/苗条");
 });
-Deno.test("From String Good 3", () => {
+Deno.test("From String Good 3", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink(`<https://one.example.com>; rel="preconnect", <https://two.example.com>; rel="preconnect", <https://three.example.com>; rel="preconnect"`);
 	assertEquals(instance.hasParameter("rel", "preconnect"), true);
 	assertEquals(instance.hasParameter("rel", "connect"), false);
@@ -24,20 +24,20 @@ Deno.test("From String Good 3", () => {
 	assertEquals(instance.getByRel("preconnect")[1][0], "https://two.example.com");
 	assertEquals(instance.getByRel("preconnect")[2][0], "https://three.example.com");
 });
-Deno.test("From String Good 4", () => {
+Deno.test("From String Good 4", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink(``);
 	assertEquals(instance.hasParameter("rel", "preconnect"), false);
 	assertEquals(instance.hasParameter("rel", "connect"), false);
 	assertEquals(instance.hasParameter("rel", "postconnect"), false);
 	assertEquals(instance.entries().length, 0);
 });
-Deno.test("From Entries Good 1", () => {
+Deno.test("From Entries Good 1", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink([["https://one.example.com", { rel: "preconnect" }]]);
 	assertEquals(instance.hasParameter("rel", "preconnect"), true);
 	assertEquals(instance.entries().length, 1);
 	assertEquals(instance.toString(), `<https://one.example.com>; rel="preconnect"`);
 });
-Deno.test("From String Bad 1", () => {
+Deno.test("From String Bad 1", { permissions: "none" }, () => {
 	assertThrows(() => {
 		new HTTPHeaderLink(`https://bad.example; rel="preconnect"`);
 	});
