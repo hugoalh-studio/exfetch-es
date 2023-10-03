e337c1827d6584b667b73c49aa9400679f970747
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-10-03 17:36:18 +0800

    Update dependencies

--- exfetch.test.ts
+++ exfetch.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.202.0/assert/assert.ts";
+import { assert } from "https://deno.land/std@0.203.0/assert/assert.ts";
 import { ExFetch } from "./exfetch.ts";
 Deno.test("https://jsonplaceholder.typicode.com/posts", {
 	permissions: {
