451943571d2aa14d88257b2fa73507e5298f3a7d
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-05 17:07:14 +0800

    Update test

--- header/link.test.ts
+++ header/link.test.ts
@@ -9,13 +9,20 @@ Deno.test("From String Good 1", { permissions: "none" }, () => {
 	assertEquals(instance.getByRel("preconnect")[0][0], "https://example.com");
 });
 Deno.test("From String Good 2", { permissions: "none" }, () => {
+	const instance = new HTTPHeaderLink(`<https://example.com>; rel=preconnect`);
+	assertEquals(instance.hasParameter("rel", "preconnect"), true);
+	assertEquals(instance.hasParameter("rel", "connect"), false);
+	assertEquals(instance.hasParameter("rel", "postconnect"), false);
+	assertEquals(instance.getByRel("preconnect")[0][0], "https://example.com");
+});
+Deno.test("From String Good 3", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink(`<https://example.com/%E8%8B%97%E6%9D%A1>; rel="preconnect"`);
 	assertEquals(instance.hasParameter("rel", "preconnect"), true);
 	assertEquals(instance.hasParameter("rel", "connect"), false);
 	assertEquals(instance.hasParameter("rel", "postconnect"), false);
 	assertEquals(instance.getByRel("preconnect")[0][0], "https://example.com/苗条");
 });
-Deno.test("From String Good 3", { permissions: "none" }, () => {
+Deno.test("From String Good 4", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink(`<https://one.example.com>; rel="preconnect", <https://two.example.com>; rel="preconnect", <https://three.example.com>; rel="preconnect"`);
 	assertEquals(instance.hasParameter("rel", "preconnect"), true);
 	assertEquals(instance.hasParameter("rel", "connect"), false);
@@ -24,7 +31,7 @@ Deno.test("From String Good 3", { permissions: "none" }, () => {
 	assertEquals(instance.getByRel("preconnect")[1][0], "https://two.example.com");
 	assertEquals(instance.getByRel("preconnect")[2][0], "https://three.example.com");
 });
-Deno.test("From String Good 4", { permissions: "none" }, () => {
+Deno.test("From String Good 5", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderLink(``);
 	assertEquals(instance.hasParameter("rel", "preconnect"), false);
 	assertEquals(instance.hasParameter("rel", "connect"), false);
