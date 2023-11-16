a26bb887006faecb9cfe7f8fdb5046bf34347e98
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-11-16 11:22:33 +0800

    Update test

--- exfetch.test.ts
+++ exfetch.test.ts
@@ -1,6 +1,7 @@
 import { assert } from "https://deno.land/std@0.206.0/assert/assert.ts";
+import { assertEquals } from "https://deno.land/std@0.206.0/assert/assert_equals.ts";
 import { ExFetch } from "./exfetch.ts";
-Deno.test("https://jsonplaceholder.typicode.com/posts", {
+Deno.test("Mono 1", {
 	permissions: {
 		net: ["jsonplaceholder.typicode.com"]
 	}
@@ -8,4 +9,17 @@ Deno.test("https://jsonplaceholder.typicode.com/posts", {
 	const response = await new ExFetch().fetch("https://jsonplaceholder.typicode.com/posts");
 	assert(response.ok === true && response.status === 200);
 	await response.body?.cancel();
+});
+Deno.test("Paginate URL 1", {
+	permissions: {
+		net: ["api.github.com"]
+	}
+}, async () => {
+	const responses = await new ExFetch().fetchPaginate("https://api.github.com/repos/microsoft/vscode/labels?per_page=100");
+	assertEquals(responses.map((response: Response) => {
+		return response.ok;
+	}).includes(false), false);
+	for (const response of responses) {
+		await response.body?.cancel();
+	}
 });
